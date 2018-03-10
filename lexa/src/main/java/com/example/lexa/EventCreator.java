6fb4eb0e0508032c8125dcb6eaa1838dba591ff1
aihummel <lexa@lexahummel.net>
2018-03-10 17:10:02 -0500

    Initializing my files

---
+++ lexa/src/main/java/com/example/lexa/EventCreator.java
@@ -0,0 +1,11 @@
+package com.example.lexa;
+
+public class EventCreator extends JPanel {
+    JTextField eventNameField, locationField, dateField, descriptionField;
+    SubmitButton submitButton;
+
+    public EventCreator(JFrame frame) {
+        this.frame = frame;
+        
+    }
+}
